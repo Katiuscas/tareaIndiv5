b9b0e2c5cc8c2cbd49d2eb47371ccd103db4ec3b
unknown <DAM127@iesataulfo.local>
2024-04-16 16:04:41 +0200

    Cuarta fase de proyecto

--- src/trabajo5IndivVeigaPedro/Clase.java
+++ src/trabajo5IndivVeigaPedro/Clase.java
@@ -4,6 +4,8 @@ package trabajo5IndivVeigaPedro;
 // Es un objeto Clase que usara la clase main para algo.
 public class Clase {
 	
-	
+	public Clase() {
+		System.out.println("Soy un objeto de la clase y acabo de ser creado");
+	}
 
 }
