7dc330a6bcc6677aa1c01f845af0a017b71ed1b0
unknown <DAM127@iesataulfo.local>
2024-04-16 15:56:42 +0200

    Segunda fase proyecto

--- src/trabajo5IndivVeigaPedro/Principal.java
+++ src/trabajo5IndivVeigaPedro/Principal.java
@@ -1,5 +1,7 @@
 package trabajo5IndivVeigaPedro;
 
+// Clase main que se encargara de llamar a los distintos metodos de la clase "Clase".
+
 public class Principal {
 
 	public static void main(String[] args) {
